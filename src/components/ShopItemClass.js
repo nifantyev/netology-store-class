e7ad63edb6abd17b07f01b9feea15018ac7cf336
Евгений Нифантьев <nifantyev@gmail.com>
2021-12-17 23:46:06 +0300

    Реализовал компонент ShopItemClass

---
+++ src/components/ShopItemClass.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ShopItem from '../models/ShopItem';
+
+const propTypes = {
+  item: PropTypes.instanceOf(ShopItem).isRequired,
+};
+
+class ShopItemClass extends React.Component {
+  constructor({ item }) {
+    super();
+    this.item = item;
+  }
+
+  render() {
+    return (
+      <div className="main-content">
+        <h2>{this.item.brand}</h2>
+        <h1>{this.item.title}</h1>
+        <h3>{this.item.description}</h3>
+        <div className="description">{this.item.descriptionFull}</div>
+        <div className="highlight-window mobile">
+          <div className="highlight-overlay"></div>
+        </div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">
+            {this.item.currency}
+            {this.item.price.toFixed(2)}
+          </div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+ShopItemClass.propTypes = propTypes;
+
+export default ShopItemClass;
